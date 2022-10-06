1a474746077cb5eb80475fb46d43e6280748b95a
LeviVitez <bmwyolo555@gmail.com>
2022-10-06 08:38:02 +0200

    Initial commit

---
+++ src/hu/petrik/statikusosztalyok/Main.java
@@ -0,0 +1,17 @@
+package hu.petrik.statikusosztalyok;
+
+public class Main {
+     /*int nemStatikusAdattag;
+    static int StatikusAdattag;
+      */
+    public static void main(String[] args) {
+        /*
+        Main objektum = new Main();
+        System.out.println(objektum.nemStatikusAdattag);
+        System.out.println(Main.StatikusAdattag);
+         */
+        for (int i = 0; i <100 ; i++) {
+            System.out.println(veletlen.velEgesz(5,10));
+        }
+    }
+}
---
+++ src/hu/petrik/statikusosztalyok/veletlen.java
@@ -0,0 +1,39 @@
+package hu.petrik.statikusosztalyok;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public final class veletlen {
+    private veletlen() {}
+
+    private static final Random rnd=new Random();
+    private static List<String > vezNevek=feltolt(("files/veznev.txt"));
+    private static List<String > ferfiKerNevek=feltolt(("file/ferfikernev.txt"));
+    private static List<String > noiKerNevek= feltolt(("file/noikernev.txt"));
+
+
+    private static List<String > feltolt(String fajlnev) {
+        List<String>lista=new ArrayList<>();
+        try {
+        Scanner file=new Scanner(new File(fajlnev));
+        while (file.hasNext()){
+            String sor = file.nextLine();
+            lista.add(sor);
+        }
+        file.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return lista;
+    }
+
+    public static int velEgesz(int min, int max){
+        return rnd.nextInt(max -min+1)+min;
+    }
+
+    }
+
